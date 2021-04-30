d72072b2016bf4e53bbaca1ae7e90f433c190253
Randy Lai <randy.cs.lai@gmail.com>
2021-04-29 23:40:08 -0700

    .

--- test/terminal.py
+++ test/terminal.py
@@ -66,42 +66,6 @@ class ByteStream(pyte.ByteStream):
         t.start()
 
 
-class Var(object):
-    def __init__(self, getter):
-        self.getter = getter
-
-    def __getattr__(self, name):
-        # fallback methods
-        def _(*args, **kwargs):
-            return Var(lambda: getattr(self.getter(), name)(*args, **kwargs))
-        return _
-
-    def observe(self, *args, **kwargs):
-        return self.getter(*args, **kwargs)
-
-    def _assert(self, op, operand, timeout=5):
-        t = time.time()
-        while time.time() - t < timeout:
-            value = self.getter()
-            if op(value, operand):
-                break
-            time.sleep(0.05)
-        else:
-            raise Exception("value is {}".format(value))
-
-    def assert_startswith(self, operand, timeout=5):
-        self._assert(six.text_type.startswith, operand, timeout)
-
-    def assert_endswith(self, operand, timeout=5):
-        self._assert(six.text_type.endswith, operand, timeout)
-
-    def assert_equal(self, operand, timeout=5):
-        self._assert(operator.eq, operand, timeout)
-
-    def assert_contains(self, operand, timeout=5):
-        self._assert(operator.contains, operand, timeout)
-
-
 class Terminal(object):
 
     def __init__(self, process, screen, stream):
@@ -121,28 +85,3 @@ class Terminal(object):
             yield cls(process, screen, stream)
         finally:
             process.terminate(force=True)
-
-    def sendintr(self):
-        self.process.sendintr()
-
-    def isalive(self):
-        return self.process.isalive()
-
-    def write(self, x):
-        self.process.write(x.encode('utf-8'))
-
-    def _line(self, num=0):
-        # parent's `line` method
-        return self.screen.display[num]
-
-    def line(self, num=0):
-        return Var(lambda: self._line(num))
-
-    def cursor(self):
-        return Var(lambda: (self.screen.cursor.x, self.screen.cursor.y))
-
-    def current_line(self):
-        return Var(lambda: self._line(self.screen.cursor.y))
-
-    def previous_line(self, num=1):
-        return Var(lambda: self._line(self.screen.cursor.y - num))
--- test/test_foo.py
+++ test/test_foo.py
@@ -3,6 +3,6 @@ from .terminal import Terminal
 
 
 def test_foo():
-    command = [sys.executable]
+    command = [sys.executable, "--version"]
     with Terminal.open(command) as terminal:
         terminal.line(0).assert_startswith("Python")
