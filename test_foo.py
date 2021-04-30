2b8e60c30767d245aa1ca79ca13321d5c34aaefb
Randy Lai <randy.cs.lai@gmail.com>
2021-04-29 23:25:08 -0700

    .

--- test_foo.py
+++ test_foo.py
@@ -4,6 +4,3 @@ from winpty import PtyProcess
 def test_foo():
     proc = PtyProcess.spawn('python')
     proc.write('print("hello world!")\r\n')
-    proc.write('exit()\r\n')
-    while proc.isalive():
-        print(proc.readline())
